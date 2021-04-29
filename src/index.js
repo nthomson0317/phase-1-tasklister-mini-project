8611be61db8fdcea863a0bab812691367db393fd
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-04-29 17:26:14 -0400

    30 minutes has passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  
 });
+
+const tasks = document.getElementById('tasks')
+
+function addToDo(){
+  const selectForm = document.getElementById('create-task-form')
+selectForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  addPTag(event.target.)
+
+}
+
+
+function addPTag(content) {
+  let li = document.createElement('li')
+  li.textContent = content
+  tasks.appendChild(li)
+}
+
